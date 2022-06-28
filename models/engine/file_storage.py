be8ef266a83af28f301add3f6c8f3f1de9e4c474
agusfl <agusflom@hotmail.com>
2022-06-28 19:44:00 +0000

    task 5 - reload and save methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,17 +35,23 @@ class FileStorage():
         """
         Serializes __objects to the JSON file (path: __file_path)
         """
+        aux_dict = {}
+        # cargar el aux_dict con la info que este en objects
+        for key, value in self.__objects.items():
+            aux_dict[key] = value.to_dict()  # se pasa el value como string
         with open(self.__file_path, mode="w", encoding="UTF-8") as json_file:
-            json.dump(self.__objects, json_file)  # REIVSAR
+            json.dump(aux_dict, json_file)
 
     def reload(self):
         """
         Deserializes the JSON file to __objects (only if the JSON file
         (__file_path) exists; otherwise, do nothing. If the file doesn’t exist,
-        no exception should be raised)
+        no exception should be raised).
         """
         if exists(self.__file_path) is True:
             with open(self.__file_path, mode="r", encoding="UTF-8") as f:
-                json.load(f)
+                aux_dict2 = json.load(f)
+            for key, value in aux_dict2.items():
+                self.__objects[key] = eval(value["__class__"])(**value)
         else:
             pass
