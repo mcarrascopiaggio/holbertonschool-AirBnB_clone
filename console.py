1b08bf810e0abc06d3bc1028f2976ae866f76af6
agusfl <agusflom@hotmail.com>
2022-06-29 13:06:45 +0000

    task 7 - create command, and add break line in EOF for when you execute the cmd in non interactive mode

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
 Entry point of the command interpreter.
 """
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -11,10 +13,13 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = "(hbnb) "
 
+    classes_list = ["BaseModel"]
+
     def do_EOF(self, arg):
         """
         Exit if EOF happens
         """
+        print()
         return True
 
     def do_quit(self, arg):
@@ -28,6 +33,23 @@ class HBNBCommand(cmd.Cmd):
         If empty line --> do nothing.
         """
         pass
+
+    def do_create(self, arg):
+        """
+        Create a new instance of BaseModel, sabes it to tje JSON file and
+        prints the "id".
+        Use of method "eval" to create an instance of the correct class if it
+        is in the list of the class attribute: "classes_list".
+        """
+        if not arg:
+            print("** class name missing **")
+        elif arg not in HBNBCommand.classes_list:
+            print("** class doesn't exist **")
+        else:
+            # create a new instance of one of the models in the list
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
 
 
 if __name__ == '__main__':
