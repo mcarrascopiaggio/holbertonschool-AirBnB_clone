f339f46ee7dec48c1a8bea0bcf6c540bf8c09d01
root <mcarrascopiaggio@gmail.com>
2022-06-29 18:25:25 -0700

    add import BaseModel

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -5,6 +5,7 @@ test Class City
 
 
 import unittest
+from models.base_model import BaseModel
 import models
 from models.city import City
 
@@ -20,16 +21,16 @@ class TestCity(unittest.TestCase):
         """
         self.assertEqual(City.name, "")
         self.assertEqual(City.state_id, "")
-        self.assertTrue(issubclass(State, BaseModel))
+        self.assertTrue(issubclass(City, BaseModel))
 
     def test_instance(self):
         """
-        test instance of state
+        test instance of City
         """
         test = City()
         self.assertEqual(test.state_id, "")
         self.assertEqual(test.name, "")
-        self.assertTrue(issubclass(State, BaseModel))
+        self.assertTrue(issubclass(City, BaseModel))
 
 
 if __name__ == "__main__":
